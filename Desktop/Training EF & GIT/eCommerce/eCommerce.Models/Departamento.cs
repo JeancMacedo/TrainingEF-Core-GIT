c19d76ef0212c139bc5f7b78c0690ccfb77fa3e4
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-07 10:05:10 -0300

    ADD - eCommerce.Models ( +Departamento.cs )

--- Desktop/Training EF & GIT/eCommerce/eCommerce.Models/Departamento.cs
+++ Desktop/Training EF & GIT/eCommerce/eCommerce.Models/Departamento.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eCommerce.Models
+﻿namespace eCommerce.Models
 {
-    internal class Departamento
+    public class Departamento
     {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public ICollection<Usuario>? Usuarios { get; set; }
     }
 }
