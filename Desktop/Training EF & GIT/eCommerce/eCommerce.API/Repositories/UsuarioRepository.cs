c367a6fdd196e23caf3226ee29c9b37b56c6dfeb
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-07 10:20:51 -0300

    ADD - UsuarioRepository on Repositories

---
+++ Desktop/Training EF & GIT/eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,32 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
